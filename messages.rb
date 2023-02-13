5840b7faec978f91a71691a817ad774be583d17d
d2gher <d2gher@gmail.com>
2023-02-13 19:04:11 +0200

    Add module for messages

---
+++ messages.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: false
+
+# Module messages for the player
+module Message
+  def welcome
+    'Welcome, try guessing the code with 4 numbers from 1 to 6'
+  end
+
+  def error
+    'Please enter 4 numbers from 1 to 6'
+  end
+end
